7ded16af49691ec4da8c8fa16407a8c490af0d3c
anh <traitimcuaanh.traitimcuamay@gmail.com>
2024-11-06 11:23:49 +0700

    display list users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,34 +1,54 @@
 const connection = require('../config/database')
-const getHomepage = (req, res) => {
-    return res.render('home.ejs')
+const { getAllUsers } = require('../services/CRUDService')
+
+const getHomepage = async (req, res) => {
+    // console.log(" >>> result: ", results)
+    let results = await getAllUsers()
+    return res.render('home.ejs', {listUsers: results})
 }
 
 const getABC = (req, res) => {
     res.render('sample.ejs')
 }
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
     // console.log(">>> req.body: ", req.body)
     let email = req.body.email
     let name = req.body.myname
     let city = req.body.city
-    
-    // console.log(">>> email= ", email, 'name = ', name, 'city = ', city)
-    // res.send('create new user')
 
-    connection.query(
+    console.log(">>> email= ", email, 'name = ', name, 'city = ', city)
+
+    let [results, fields] = await connection.query(
         `INSERT INTO 
         Users (email, name, city)
         VALUES (?, ?, ?);`,
-        [email, name, city],
-        function(err, results) {
-          console.log(results);
-          res.send(' Created user succeed !')
-        }
+        [email, name, city]
     )
+
+    console.log(" >>> result: ", results)
+    res.send(' Created user succeed !')
+
+    // connection.query(
+    //     `INSERT INTO 
+    //     Users (email, name, city)
+    //     VALUES (?, ?, ?);`,
+    //     [email, name, city],
+    //     function(err, results) {
+    //       console.log(results);
+    //       res.send(' Created user succeed !')
+    //     }
+    // )
+    // const [results, fields] = await connection.query('select * from Users');
+    // console.log(" >>> result: ", results)
 }
+
+const getCreatePage = (req, res) => {
+    res.render('create.ejs')
+}
+
 
 module.exports = {
     getHomepage, getABC,
-    postCreateUser
+    postCreateUser, getCreatePage
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,13 @@
 // khai bao route
 const express = require('express')
-const { getHomepage, getABC, postCreateUser } = require('../controllers/homeController')
+const { getHomepage, getABC, postCreateUser, getCreatePage } = require('../controllers/homeController')
 const router = express.Router()
 
 // router.Method('/route',handler)
 router.get('/', getHomepage)
 
 router.get('/abc', getABC)
-
+router.get('/create', getCreatePage)
 router.post('/create-user', postCreateUser)
 
 module.exports = router
